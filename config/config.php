0f1fed967475e5b4a7f86c125767e21049500e24
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2016-10-14 16:10:56 -0600

    fix

--- config/config.php
+++ config/config.php
@@ -23,11 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
 $config['base_url'] = 'http://localhost:8080';
-=======
-$config['base_url'] = 'http://localhost/';
->>>>>>> 689075f0d1113b1aad60f815e8f9c76038b88344
+
 
 /*
 |--------------------------------------------------------------------------
